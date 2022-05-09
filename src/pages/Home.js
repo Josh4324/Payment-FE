6441f06b70b9728e06e86d540f4ea04cec2fcd70
Josh <adesanyajoshua@ymail.com>
2022-05-09 18:56:12 +0100

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import WalletConnectProvider from "@walletconnect/web3-provider";
 import axios from "axios";
 
 export default function Home() {
-  const contractAddress = "0x62e2c7165Df60B3D079cAADfA11B769ea9CcB28E";
+  const contractAddress = "0xE68C05dD79bF80f958AD4D453142b9Ad099b825B";
 
   const contractABI = abi.abi;
   const [loading, setLoading] = useState(false);
@@ -25,19 +25,324 @@ export default function Home() {
   const etherAmountRef = useRef();
   const walletRef = useRef();
 
-  const getProviderOrSigner = async (needSigner = false) => {};
+  const web3ModalRef = useRef();
+  let provider;
+  let web3Modal;
 
-  const sendEthToAnotherAddr = async () => {};
+  const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider, // required
+      options: {
+        infuraId: "c42bd204d32641289769eb8a9a4c1607", // required
+      },
+    },
+    binancechainwallet: {
+      package: true,
+    },
+  };
 
-  const sendEthToContract = async () => {};
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const { ethereum } = window;
 
-  const getPrice = async () => {};
+    if (!ethereum) {
+      alert("Get MetaMask!");
+      return;
+    }
 
-  const getBalance = async () => {};
+    web3Modal = new Web3Modal({
+      network: "rinkeby", // optional
+      cacheProvider: true, // optional
+      providerOptions, // required
+    });
 
-  const connectWallet = async () => {};
+    provider = await web3Modal.connect();
+    const web3Provider = new providers.Web3Provider(provider);
 
-  const disconnectWallet = async () => {};
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    setAddress(accounts[0]);
+
+    // If user is not connected to the Mainnet network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    // rinkbey - 4
+    // bsc - 97
+    // polygon - 80001
+    if (chainId !== 4) {
+      window.alert("Change the network to Rinkeby");
+      throw new Error("Change network to rinkeby");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  const sendEthToAnotherAddr = async () => {
+    setError("");
+    setMessage("");
+    const { ethereum } = window;
+    if (!ethereum) {
+      return alert("Get MetaMask!");
+    }
+
+    if (walletRef.current.value === "") {
+      return setError("Please enter your wallet address");
+    }
+    if (walletRef.current.value.length !== 42) {
+      return setError("Please enter a valid wallet address");
+    }
+
+    if (ethers.utils.isAddress(walletRef.current.value) === false) {
+      return setError("Please enter a valid wallet address");
+    }
+
+    if (etherRef.current.value === "") {
+      return setError("Please enter ether amount");
+    }
+
+    if (etherRef.current.value <= 0) {
+      return setError("You cant send zero or less than zero ethers");
+    }
+
+    if (ethereum) {
+      setLoading(true);
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+
+      const paymentContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      const walletAddress = walletRef.current.value;
+      const ethAmount = etherRef.current.value;
+
+      try {
+        const Txn = await paymentContract.sendPayment(walletAddress, {
+          gasLimit: 300000,
+          value: ethers.utils.parseEther(ethAmount),
+        });
+
+        await Txn.wait();
+
+        setLoading(false);
+        setMessage("Payment was successful");
+        const prov = await getProviderOrSigner();
+        const bal = await prov.getBalance(address);
+        setBalance(Number(BigNumber.from(bal)) / 10 ** 18);
+
+        walletRef.current.value = "";
+        etherRef.current.value = "";
+      } catch (error) {
+        if (error.code) {
+          console.log(error.toString);
+          setError(error.message);
+        } else {
+          setError(error.toString());
+          console.log(error.toString());
+        }
+
+        setLoading(false);
+
+        walletRef.current.value = "";
+        etherRef.current.value = "";
+      }
+    } else {
+      setLoading(false);
+      window.alert("Please connect to metamask");
+      console.log("Ethereum object doesn't exist!");
+    }
+  };
+
+  const sendEthToContract = async () => {
+    setError1("");
+    setMessage1("");
+    const { ethereum } = window;
+    if (!ethereum) {
+      return alert("Get MetaMask!");
+    }
+
+    if (etherAmountRef.current.value === "") {
+      return setError1("Please enter ether amount");
+    }
+
+    if (etherAmountRef.current.value <= 0) {
+      return setError1("You cant send zero or less than zero ethers");
+    }
+
+    if (ethereum) {
+      setLoading1(true);
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+
+      const paymentContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      const ethAmount = etherAmountRef.current.value;
+
+      try {
+        const Txn = await paymentContract.sendToContract({
+          gasLimit: 300000,
+          value: ethers.utils.parseEther(ethAmount),
+        });
+
+        await Txn.wait();
+
+        setLoading1(false);
+        setMessage1("Payment was successful");
+        const prov = await getProviderOrSigner();
+        const bal = await prov.getBalance(address);
+        setBalance(Number(BigNumber.from(bal)) / 10 ** 18);
+
+        walletRef.current.value = "";
+        etherRef.current.value = "";
+      } catch (error) {
+        if (error.code) {
+          console.log(error.toString);
+          setError1(error.message);
+        } else {
+          setError1(error.toString());
+          console.log(error.toString());
+        }
+
+        setLoading1(false);
+
+        walletRef.current.value = "";
+        etherRef.current.value = "";
+      }
+    } else {
+      setLoading1(false);
+      window.alert("Please connect to metamask");
+      console.log("Ethereum object doesn't exist!");
+    }
+  };
+
+  const getPrice = async () => {
+    const data = await axios.get(
+      "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+    );
+
+    setPrice(data.data.ethereum.usd);
+  };
+
+  const getBalance = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        //setLoading(true);
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+
+        const paymentContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        const contractBalance = await paymentContract.getBalance();
+        setContractBalance(Number(BigNumber.from(contractBalance)) / 10 ** 18);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Get the provider from web3Modal, which in our case is MetaMask
+        // When used for the first time, it prompts the user to connect their wallet
+        await getProviderOrSigner();
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+        setAddress(accounts[0]);
+        const prov = await getProviderOrSigner();
+        const bal = await prov.getBalance(accounts[0]);
+
+        setBalance(Number(BigNumber.from(bal)) / 10 ** 18);
+
+        // track when wallet is connected
+        localStorage.setItem("wall", "true");
+
+        // Subscribe to accounts change
+        provider.on("accountsChanged", async (accounts) => {
+          const provider = await getProviderOrSigner();
+          const bal = await provider.getBalance(accounts[0]);
+          setBalance(Number(BigNumber.from(bal)) / 10 ** 18);
+        });
+
+        // Subscribe to chainId change
+        provider.on("chainChanged", async (chainId) => {
+          console.log("chain", chainId);
+          await getProviderOrSigner();
+        });
+
+        // Subscribe to provider connection
+        provider.on("connect", async (info) => {
+          console.log("inf", info);
+        });
+
+        // Subscribe to provider disconnection
+        provider.on("disconnect", (error) => {
+          console.log("dis", error);
+          setAddress("");
+        });
+      } else {
+        alert("Please install Metamask");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const disconnectWallet = async () => {
+    localStorage.removeItem("wall");
+    web3Modal = new Web3Modal({
+      network: "rinkeby", // optional
+      cacheProvider: true, // optional
+      providerOptions, // required
+    });
+    await web3Modal.clearCachedProvider();
+    setAddress("");
+  };
+
+  useEffect(() => {
+    console.log("change");
+    const { ethereum } = window;
+
+    if (ethereum) {
+      if (localStorage.getItem("wall")) {
+        // Assign the Web3Modal class to the reference object by setting it's `current` value
+        // The `current` value is persisted throughout as long as this page is open
+        web3ModalRef.current = new Web3Modal({
+          network: "rinkeby",
+          providerOptions: {},
+          disableInjectedProvider: false,
+        });
+
+        connectWallet();
+      }
+      setBalance(0);
+    }
+
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+  }, [address]);
+
+  useEffect(() => {
+    getBalance();
+    getPrice();
+  }, []);
 
   return (
     <div
